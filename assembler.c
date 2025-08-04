1f8afd44b7e544763f8f8998fd7ee2c6c1dadd72
Osher <dsaewqcxz1@gmail.com>
2025-08-04 20:26:36 +0300

    Updated CMakeLists.txt

---
+++ assembler.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int  i;
+    i = 5;
+    printf("Hello, World! %d\n" , i);
+    return 0;
+}
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, World!\n");
-    printf("This is a simple C program.\n");
-    return 0;
-}
