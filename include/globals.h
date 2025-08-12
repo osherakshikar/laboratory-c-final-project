d09580f2d99fe0d222b733f1b21494ca490bfdd0
Osher <dsaewqcxz1@gmail.com>
2025-08-12 19:21:00 +0300

    adjusted globals.h

--- include/globals.h
+++ include/globals.h
@@ -1,5 +1,7 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * @enum bool_t
@@ -10,20 +12,14 @@ typedef enum {
     TRUE = 1
 } bool_t;
 
-char *dupstr(const char *str) {
-    char *dup = malloc(strlen(str) + 1);
-    if (dup) {
-        strcpy(dup, str);
-    }
-    return dup;
-}
+char *dupstr(const char *str);
 
 #ifndef MAX_STRING_LEN
-#define MAX_STRING_LEN 256      /* fits any single input line */
+#define MAX_STRING_LEN 80      /* fits any single input line */
 #endif
 
 #ifndef MAX_DATA_ITEMS
-#define MAX_DATA_ITEMS 128      /* .data numbers per line */
+#define MAX_DATA_ITEMS 32      /* .data numbers per line */
 #endif
 
 #ifndef MAX_MATRIX_ROWS
@@ -34,7 +30,7 @@ char *dupstr(const char *str) {
 #endif
 #define MAX_MATRIX_CELLS (MAX_MATRIX_ROWS * MAX_MATRIX_COLS)
 
-#define MAX_LINE_LENGTH 81  /* 80 chars + terminator */
+#define MAX_LINE_LENGTH 82 /* 80 chars + newline / terminator */
 #define MAX_LABEL_LENGTH  31  /* 30 chars + terminator */
 
 #endif
